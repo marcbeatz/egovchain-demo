a52b10690ff20439cae529efb26c282e40464962
marcbeatz <marclouie06@gmail.com>
2024-12-07 09:55:00 +0800

    :bug: build config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/egovchain-demo',
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: '/egovchain-demo/',
+  // output: 'export',
+  // basePath: '/egovchain-demo',
+  // images: {
+  //   unoptimized: true,
+  // },
+  // assetPrefix: '/egovchain-demo/',
 };
 
 module.exports = nextConfig;
